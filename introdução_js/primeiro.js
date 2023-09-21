20e7900b7e7ce461ad27966bad0f053abf014e6c
Gustavo Messias Santos <gustavopessoal2023@gmail.com>
2023-09-21 17:46:40 -0300

    Aula 21/09/2023

---
+++ introdução_js/primeiro.js
@@ -0,0 +1,40 @@
+
+var numero = 10
+var numero = 15
+
+let num1 = 15
+num1 = 10
+
+function pesadelo()
+{
+    var a = 15
+    var b = 20
+}
+
+a = 10
+b = 25
+
+var nome = "Luan Muruk"
+var nome2 = 'Luan'
+
+console.log(typeof(nome))
+
+let x = '12'
+let y = Number(x)
+
+console.log(Number.isInteger(y))
+
+//x = 12.5984
+//console.log(x.tofixed(2))
+
+var x1 = 1
+var x2 = '2'
+console.log(x1+x2)
+console.log('15' + 2)
+console.log('aula' * 2)
+console.log(0.9 - 0.5)
+
+const PI = 3.1441
+
+const area = Math.PI * Math.pow(PI, 2)
+console.log(area)
---
+++ introdução_js/segundo_js/js/script.js
@@ -0,0 +1,2 @@
+//alert("Você entrou no cite serto! Aprenda portugueis")
+//document.getElementsByTagName('h1').innerHTML = "Esse h1 foi alterado"
